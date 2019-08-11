e2a030a2e0b7713db42cb0e507a6203ac3258255
Sandy Maguire <sandy@sandymaguire.me>
2019-08-11 00:24:36 -0700

    pattern match underway

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs            #-}
 {-# LANGUAGE KindSignatures   #-}
+{-# LANGUAGE PatternSynonyms  #-}
+{-# LANGUAGE ViewPatterns     #-}
 
 module Lib where
 
@@ -14,6 +16,15 @@ import HsSyn
 import RdrName
 import SrcLoc
 import OccName
+import Control.Lens
+import Control.Lens.Plated
+import Data.Data.Lens
+import BasicTypes
+
+unapply :: OccName -> HsExpr GhcPs -> HsExpr GhcPs
+unapply occ (HsApp _ (L _ (HsVar _ (L _ (Unqual occ')))) (L _ z))
+  | occ == occ' = z
+unapply _ z = z
 
 class Foo (a :: * -> *) where
   janky :: a x
@@ -24,18 +35,40 @@ instance Foo (M1 _1 _2 _3) where
 inprogressOcc :: OccName
 inprogressOcc = mkVarOcc "inprogress"
 
+underwayOcc :: OccName
+underwayOcc = mkVarOcc "underway"
+
+
+test :: Int
+test = underway 1 (underway 0 (13) + 5)
 
 findInProgress :: [LHsDecl GhcPs] -> [LHsDecl GhcPs]
-findInProgress = filter (everything (||) $ mkQ False f2)
-  where
-    f2 :: HsExpr GhcPs -> Bool
-    f2 (HsVar _ (L _ (Unqual occ))) = occ == inprogressOcc
-    f2 _ = False
+findInProgress = filter (everything (||) $ mkQ False $ f2 underwayOcc)
+
+pattern Underway :: Integer -> HsExpr GhcPs -> HsExpr GhcPs
+pattern Underway i activity
+  <- HsApp _ (L _ (HsApp _ (L _ ( HsVar _ (L _ (Unqual ((== underwayOcc) -> True)))))
+                           (L _ (HsOverLit _ (OverLit _ (HsIntegral (IL _ False i)) _)))))
+             (L _ activity)
+
+unUnderway :: HsExpr GhcPs -> HsExpr GhcPs
+unUnderway (Underway 0 z) = z
+unUnderway a = a
+
+
+f2 :: OccName -> HsExpr GhcPs -> Bool
+f2 occ (HsVar _ (L _ (Unqual occ'))) = occ == occ'
+f2 _ _ = False
 
 
 main :: IO ()
 main = do
   Right (anns, (L _ a)) <- parseModule "src/Lib.hs"
   let inprog = findInProgress $ hsmodDecls a
-  for_ inprog $ pprTraceM "hello" . ppr
+  for_ inprog $ \p -> do
+    pprTraceM "found" $ ppr p
+    pprTraceM "replaced" . ppr $ everywhere (mkT unUnderway) p
+     -- p & biplate . filtered f2 .~ HsUnboundVar NoExt (TrueExprHole $ mkVarOcc "_yo")
+      -- p & partsOf (biplate . filtered f2) . reversed . _head .~ HsUnboundVar NoExt (TrueExprHole $ mkVarOcc "_yo")
+     -- p & biplate . filtered f2 .~ HsUnboundVar NoExt (TrueExprHole $ mkVarOcc "_yo")
 
--- src/Markers.hs
+++ src/Markers.hs
@@ -3,3 +3,6 @@ module Markers where
 inprogress :: a
 inprogress = error "in progress"
 
+underway :: Int -> a -> a
+underway = error "underway"
+
